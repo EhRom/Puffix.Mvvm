11a615edff18b0d6d1b11fed3b891fcff75f7be8
EhRom <romain_ehret@hotmail.com>
2024-01-23 21:00:26 +0100

    Add contracts for pages and for ViewModel with Model

---
+++ Puffix.Mvvm/Pages/IPage.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel;
+
+namespace Puffix.Mvvm.Pages;
+
+/// <summary>
+/// Base contract for pages.
+/// </summary>
+public interface IPage : INotifyPropertyChanged
+{ }
--- Puffix.Mvvm/ViewModels/IViewModel.cs
+++ Puffix.Mvvm/ViewModels/IViewModel.cs
@@ -1,7 +1,18 @@
-﻿namespace Puffix.Mvvm.ViewModels;
+﻿using Puffix.Mvvm.Models;
+
+namespace Puffix.Mvvm.ViewModels;
 
 /// <summary>
 /// Base contract for view-models.
 /// </summary>
 public interface IViewModel
 { }
+
+/// <summary>
+/// Base contract for view-models.
+/// </summary>
+public interface IViewModel<ModelT>
+    where ModelT : IModel
+{
+    ModelT Model { get; }
+}
